0893cd06477562ca7313642411304bc8d63f8df6
1234Tasneem1234 <12tasnimtasnim@gmail.com>
2021-08-29 07:45:07 +0600

    more user information and styles added

--- script.js
+++ script.js
@@ -15,4 +15,16 @@ const displayUser = randomUser => {
         <img src = ${imageUrl}>
         <h4>User-Name: ${randomUser.results[0].name.title} ${randomUser.results[0].name.first} ${randomUser.results[0].name.last}</h4>
         <p>User-Location: ${userLocation.street.number} ${userLocation.street.name} ${userLocation.city} ${userLocation.state} ${userLocation.country}</p>
+        <p>User-Email: ${randomUser.results[0].email}</p>
+        <p>User-Phone Number: ${randomUser.results[0].phone}</p>
+        <p>User-Gender: ${randomUser.results[0].gender}</p>
+        <p>User-Age: ${randomUser.results[0].dob.age}</p>
+        <p>User-Register-Date: ${randomUser.results[0].registered.date}</p>
     `}
+
+const div = document.getElementsByTagName('div');
+for (const userContainer of div) {
+    userContainer.style.border = '3px solid rgb(230, 26, 234)';
+    userContainer.style.padding = '15px';
+}
+
